2193b5bdca2deffc99454c1e40549f68579c9769
Wangariiisharon <mwangiiisharon@gmail.com>
2023-07-21 00:54:12 +0300

    Added firebase config

---
+++ src/firebase/configs.ts
@@ -0,0 +1,34 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, Timestamp } from "firebase/firestore";
+
+const apps = getApps();
+
+if (!apps.length || apps.length === 0) {
+  console.debug("do initialize firebase...");
+
+  try {
+    const firebaseConfig = {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    };
+
+    initializeApp(firebaseConfig);
+  } catch (error) {
+    console.error("FIREBASE INIT ERROR:::", error);
+  }
+}
+
+const firebaseApp = getApps()[0];
+// console.log('firebaseApp:', firebaseApp);
+
+export const fbAuth = getAuth(firebaseApp);
+export const fbDb = getFirestore(firebaseApp);
+export const fromMillis = Timestamp.fromMillis;
+
+export default firebaseApp;
