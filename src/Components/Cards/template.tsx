ed6ef4236ff2be30dbb22fc146f27e3849a3de86
Sharon <sharon@gessic.com>
2024-04-17 04:22:53 +0300

    Group Permissions

--- src/Components/Cards/template.tsx
+++ src/Components/Cards/template.tsx
@@ -2,128 +2,127 @@ import { DocumentData } from "firebase/firestore";
 
 interface ExampleProps {
   heading: string;
-  list: DocumentData[];
+  list: string[];
+  action: any;
 }
-
-// export const Example: React.FC<ExampleProps> = ({ heading, list }) => {
-//   return (
-//     <div className="shadow-md bg-[#FFFFFF] w-2/5 rounded-md h-64 px-2 py-2">
-//       <div className="flex justify-between">
-//         <div className="sm:flex-auto">
-//           <h1 className="text-base font-semibold leading-6 text-gray-900 sticky">
-//             {heading}
-//           </h1>
-//         </div>
-//         <div className="mt-4 lg sm:ml-16 sm:mt-0 sm:flex-none">
-//           <button
-//             type="button"
-//             className="block rounded-md bg-[#4FD1C5] px-3 py-1 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-//           >
-//             Add +
-//           </button>
-//         </div>
-//       </div>
-//       <div
-//         className="overflow-x-auto"
-//         style={{ maxHeight: "calc(100vh - 200px)" }}
-//       >
-//         <table className="w-full divide-y divide-gray-300">
-//           <thead className="sticky top-0 bg-white">
-//             <tr>
-//               <th className="whitespace-nowrap py-1 px-1 text-sm font-medium text-gray-900">
-//                 Name
-//               </th>
-//               <th className="whitespace-nowrap py-1 px-1 text-right text-sm font-medium">
-//                 Actions
-//               </th>
-//             </tr>
-//           </thead>
-//           <tbody className="divide-y divide-gray-200 bg-white">
-//             {list.map((permission, index) => (
-//               <tr key={index} className="bg-[#FAFAFB]">
-//                 <td className="whitespace-nowrap py-1 px-1 text-sm font-medium text-gray-900">
-//                   {permission.name}
-//                 </td>
-//                 <td className="whitespace-nowrap py-1 px-1 text-right text-sm font-medium">
-//                   <a href="#" className="text-red-600 hover:text-red-900">
-//                     <span className="fa-stack fa-lg">
-//                       <i className="fa fa-square fa-stack-2x"></i>
-//                       <i className="fa fa-times fa-stack-1x fa-inverse"></i>
-//                     </span>
-//                   </a>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };import { DocumentData } from "firebase/firestore";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
-
-export const Example: React.FC<ExampleProps> = ({ heading, list }) => {
-  const isPermissions = heading === "Permissions";
-
+export const Example: React.FC<ExampleProps> = ({ heading, list, action }) => {
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
-            {heading}
-          </h1>
+    <div className="shadow-md bg-[#FFFFFF] rounded-md overflow-y-auto h-64">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center  mb-6">
+          <div className="sm:flex-auto">
+            <h1 className="text-base font-semibold leading-6 text-gray-900">
+              {heading}
+            </h1>
+          </div>
         </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          {isPermissions ? (
-            <button
-              type="button"
-              className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Add
-            </button>
-          ) : null}
+        <div className="flow-root">
+          <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle">
+              <table className="min-w-full border-separate border-spacing-0">
+                <tbody>
+                  {list.map((permission: string, index: number) => (
+                    <tr key={index} className="bg-[#FAFAFB]">
+                      <td
+                        className={classNames(
+                          index !== list.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+                        )}
+                      >
+                        {permission}
+                      </td>
+                      <td
+                        className={classNames(
+                          index !== list.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8"
+                        )}
+                        onClick={() => action(permission)}
+                      >
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Remove
+                          <span className="sr-only">, {permission}</span>
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="mt-8 flow-root">
-        <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle">
-            <table className="min-w-full border-separate border-spacing-0">
-              <tbody>
-                {list.map((permission: any, index: any) => (
-                  <tr key={index} className="bg-[#FAFAFB]">
-                    <td
-                      className={classNames(
-                        index !== permission.length - 1
-                          ? "border-b border-gray-200"
-                          : "",
-                        "whitespace-nowrap px-3 py-4 text-sm text-gray-500"
-                      )}
-                    >
-                      {permission.name}
-                    </td>
-                    <td
-                      className={classNames(
-                        index !== permission.length - 1
-                          ? "border-b border-gray-200"
-                          : "",
-                        "relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8"
-                      )}
-                    >
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
+    </div>
+  );
+};
+
+interface AllPermissionsProps {
+  heading: string;
+  list: DocumentData[];
+  action: any;
+}
+export const AllPermissions: React.FC<AllPermissionsProps & {}> = ({
+  heading,
+  list,
+  action,
+}) => {
+  return (
+    <div className="shadow-md bg-[#FFFFFF] rounded-md overflow-y-auto h-64">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-base font-semibold leading-6 text-gray-900">
+              {heading}
+            </h1>
+          </div>
+        </div>
+        <div className="mt-6 flow-root">
+          <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle">
+              <table className="min-w-full border-separate border-spacing-0">
+                <tbody>
+                  {list.map((permission: any, index: any) => (
+                    <tr key={permission.id} className="bg-[#FAFAFB] px-2">
+                      <td
+                        className={classNames(
+                          index !== permission.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+                        )}
                       >
-                        {isPermissions ? "Add" : "Edit"}
-                        <span className="sr-only">, {permission.name}</span>
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+                        {permission.name}
+                      </td>
+                      <td
+                        className={classNames(
+                          index !== permission.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8"
+                        )}
+                      >
+                        <button
+                          onClick={() => action(permission.id)}
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Add
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Administration/Admins/manage_department/viewDepatment.tsx
+++ src/pages/Administration/Admins/manage_department/viewDepatment.tsx
@@ -4,6 +4,8 @@ import {
   doc,
   getDoc,
   getDocs,
+  onSnapshot,
+  updateDoc,
 } from "firebase/firestore";
 import React, { Fragment, useEffect, useState } from "react";
 import { fbDb } from "@/firebase/configs";
@@ -12,8 +14,13 @@ import SiteLayout from "@/Layout/SiteLayout";
 import { PlusIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Button } from "@/components/Buttons";
 import { Example } from "@/components/Cards/template";
+import { AllPermissions } from "@/components/Cards/template";
 import { BodyCell } from "@/components/Table/Cells";
 import { formatDistanceToNow } from "date-fns";
+import toast from "react-hot-toast";
+
+import { FieldValue } from "firebase/firestore";
+
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -25,27 +32,13 @@ import Paper from "@mui/material/Paper";
 interface DepatmentDetailsProps {
   departmentId: string;
   department: {
+    departmentId?: string; // Make departmentId optional
     name: string;
     members: number;
-    permissions: number;
+    permissions: string[];
     update: Date;
   };
 }
-
-function createData(
-  id: string,
-  name: string,
-  AddedAt: string,
-  Action: React.ReactNode | null
-) {
-  return { name, id, AddedAt, Action };
-}
-
-const action = (
-  <i className="fa-sharp fa-solid fa-rectangle-xmark text-[#F34C4C] text-sm"></i>
-);
-
-const rows = [createData("11", "Dina Morad", "6 months ago", action)];
 
 export default function ViewDepatment() {
   const [departments, setdepartments] = useState<
@@ -57,56 +50,148 @@ export default function ViewDepatment() {
   const [fetchedPermisions, setFetchedPermisions] = useState<DocumentData[]>(
     []
   );
+  const [departmentPermissions, setDepartmentPermissions] = useState<
+    DocumentData[]
+  >([]);
+
+  const [loading, setLoading] = useState(true);
+
   const router = useRouter();
   const { id } = router.query;
+  console.log("This is the ID", id);
+
   function handleAddPermissions() {}
   function handleAddMembers() {}
 
-  useEffect(() => {
-    const fetchDepartmentDetails = async () => {
-      if (id) {
-        try {
-          const depatmentDocRef = doc(fbDb, "departments", id as string);
-          const depatmentDocSnap = await getDoc(depatmentDocRef);
+  // Function to add a permission to the department
+  // Function to add a permission to the department
 
-          if (depatmentDocSnap.exists()) {
-            const depatmentData =
-              depatmentDocSnap.data() as DepatmentDetailsProps["department"];
-            console.log(depatmentData);
+  async function addPermission(departmentId: string, permissionId: string) {
+    console.log("permissionId", permissionId);
+    console.log("departmentId", departmentId);
 
-            setdepartments(depatmentData);
-          } else {
-            console.log("Department not found");
-          }
-        } catch (error) {
-          console.error("Error fetching Department:", error);
+    try {
+      const departmentRef = doc(fbDb, "departments", departmentId);
+      const departmentDocSnap = await getDoc(departmentRef);
+
+      if (departmentDocSnap.exists()) {
+        let permissions: string[] | undefined =
+          departmentDocSnap.data()?.permissions;
+
+        // If permissions array is undefined or null, initialize it as an empty array
+        if (!permissions) {
+          permissions = [];
         }
-      }
-    };
 
+        // Retrieve the name of the permission document
+        const permissionDocRef = doc(fbDb, "permisions", permissionId);
+        console.log("permissionDocRef", permissionDocRef);
+
+        const permissionDocSnap = await getDoc(permissionDocRef);
+
+        if (permissionDocSnap.exists()) {
+          const permissionName = permissionDocSnap.data()?.name;
+
+          // Check if the permissions array is not undefined before calling includes()
+          if (permissions && !permissions.includes(permissionName)) {
+            permissions.push(permissionName); // Add the new permission name
+            await updateDoc(departmentRef, { permissions }); // Update the permissions array in the document
+            console.log("Permission added successfully");
+            toast.success("Permission added successfully");
+          } else {
+            console.log("Permission already exists");
+            toast.error("Permission already exists");
+          }
+        } else {
+          console.log("Permission document not found");
+          toast.error("Permission document not found");
+        }
+      } else {
+        console.log("Department not found");
+        toast.error("Department not found");
+      }
+    } catch (error) {
+      console.error("Error adding permission:", error);
+      toast.error("Error adding permission");
+    }
+  }
+  // Function to remove a permission from the department
+  async function removePermission(departmentId: string, permissionId: string) {
+    console.log("permissionId", permissionId);
+    console.log("departmentId", departmentId);
+    try {
+      const departmentRef = doc(fbDb, "departments", departmentId);
+      const departmentDocSnap = await getDoc(departmentRef);
+      if (departmentDocSnap.exists()) {
+        let permissions: string[] = Array.isArray(
+          departmentDocSnap.data().permissions
+        )
+          ? departmentDocSnap.data().permissions
+          : [];
+
+        if (!Array.isArray(permissions)) {
+          permissions = [];
+        }
+
+        const index = permissions.indexOf(permissionId);
+        if (index !== -1) {
+          permissions.splice(index, 1);
+          await updateDoc(departmentRef, { permissions });
+          console.log("Permission removed successfully");
+          toast.success("Permission removed successfully");
+        } else {
+          console.log("Permission not found");
+          toast.error("Permission not found");
+        }
+      } else {
+        console.log("Department not found");
+        toast.error("Department not found");
+      }
+    } catch (error) {
+      console.error("Error removing permission:", error);
+      toast.error("Error removing permission");
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = () => {}; // Initialize unsubscribe function
+
+    if (id) {
+      const departmentId = id as string;
+      const depatmentDocRef = doc(fbDb, "departments", departmentId);
+
+      // Subscribe to changes using onSnapshot
+      const unsubscribe = onSnapshot(depatmentDocRef, (docSnapshot) => {
+        if (docSnapshot.exists()) {
+          const departmentData =
+            docSnapshot.data() as DepatmentDetailsProps["department"];
+          setdepartments(departmentData);
+        }
+      });
+    }
+
+    // Fetch permissions data once
     const fetchPermissions = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(fbDb, "permisions"));
-        const permissionsData: DocumentData[] = [];
-        console.log(permissionsData);
+      const querySnapshot = await getDocs(collection(fbDb, "permisions"));
+      const permissionsData: DocumentData[] = [];
 
-        querySnapshot.forEach((doc) => {
-          const permission = {
-            id: doc.id,
-            ...doc.data(),
-          };
-          permissionsData.push(permission);
-        });
-        setFetchedPermisions(permissionsData);
-      } catch (error) {
-        console.error("Error fetching permissions:", error);
-      }
+      querySnapshot.forEach((doc) => {
+        const permission = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        permissionsData.push(permission);
+      });
+      setFetchedPermisions(permissionsData);
     };
 
-    fetchDepartmentDetails();
     fetchPermissions();
+
+    // Clean up function to unsubscribe from snapshot listener
+    return () => {
+      unsubscribe();
+    };
   }, [id]);
-  console.log("Department Update Date:", departments?.update);
 
   return (
     <>
@@ -158,20 +243,21 @@ export default function ViewDepatment() {
                 </div>
               </div>
             </div>
-            <div className="flex justify-between mt-10">
-              <div>
-                <div className="">
-                  <PermissionsTable
-                    permissions={fetchedPermisions}
-                    heading={departments?.name ?? "Default Heading"}
-                  />
-                </div>
+            <div className="mt-10 flex flex-row ml-5 w-full">
+              <div className="w-1/2 pr-4">
+                <PermissionsTable
+                  permissions={departments?.permissions ?? []}
+                  heading={departments?.name ?? "Default Heading"}
+                  func={removePermission}
+                  departmentId={id as string}
+                />
               </div>
-
-              <div>
+              <div className="w-1/2 pl-4">
                 <AllPermissionsTable
                   permissions={fetchedPermisions}
-                  heading="Permisions"
+                  heading="Permissions"
+                  func={addPermission}
+                  departmentId={id as string}
                 />
               </div>
             </div>
@@ -183,39 +269,68 @@ export default function ViewDepatment() {
 }
 
 interface PermissionsTableProps {
-  permissions: DocumentData[];
+  permissions: string[];
   heading: string;
+  func: any;
+  departmentId: string;
+}
+interface PermissionsTableProps {
+  permissions: string[];
+  heading: string;
+  func: any;
+  departmentId: string;
 }
 
-function PermissionsTable({ permissions, heading }: PermissionsTableProps) {
-  return <Example heading={heading} list={permissions}></Example>;
+function PermissionsTable({
+  permissions,
+  heading,
+  func,
+  departmentId,
+}: PermissionsTableProps) {
+  const handleRemovePermission = (permissionId: string) => {
+    console.log("Handle Remove Permission: " + permissionId);
+
+    if (departmentId && permissionId) {
+      func(departmentId, permissionId);
+    } else {
+      console.error("PermissionId or DepartmentId is undefined");
+    }
+  };
+
+  return (
+    <Example
+      heading={heading}
+      list={permissions}
+      action={handleRemovePermission}
+    />
+  );
 }
+
 interface AllPermissionsTableProps {
   permissions: DocumentData[];
   heading: string;
+  func: any;
+  departmentId: string; // Change the type to string
 }
 function AllPermissionsTable({
   permissions,
   heading,
+  func,
+  departmentId,
 }: AllPermissionsTableProps) {
-  return <Example heading={heading} list={permissions}></Example>;
-}
-// interface PermissionsTableProps {
-//   permissions: DocumentData[];
-// }
+  const handleAddPermission = (permissionId: string) => {
+    if (departmentId && permissionId) {
+      func(departmentId, permissionId);
+    } else {
+      console.error("PermissionId or DepartmentId is undefined");
+    }
+  };
 
-// export function PermissionsTable({ permissions }: PermissionsTableProps) {
-//   return (
-//     <Table sx={{ minWidth: 400 }} aria-label="simple table">
-//       <tbody>
-//         {permissions.map((permission, index) => {
-//           return (
-//             <tr key={index} className="h-10">
-//               <td className="ml-2">{permission.name}</td>
-//             </tr>
-//           );
-//         })}
-//       </tbody>
-//     </Table>
-//   );
-// }
+  return (
+    <AllPermissions
+      heading={heading}
+      list={permissions}
+      action={handleAddPermission} // Pass the handler directly
+    />
+  );
+}
