15d82c7d28ecb510b251fa316bb1a16f2dd5ee93
Wangariiisharon <mwangiiisharon@gmail.com>
2024-06-07 10:46:26 +0300

    update DashLogo component

--- src/components/images/index.tsx
+++ src/components/images/index.tsx
@@ -1,25 +1,14 @@
-import darkLogo from '../../../public/Darklogo.png'
-import dashlogo from '../../../public/dashlogo.png' 
-
-
 import Image from "next/image";
 
-
 interface Props {
-    src: string
-    alt: string
+  src: string;
+  alt: string;
 }
 
-export function CardIcon({src, alt}:Props){
-    return (
-        <Image src={src} alt={alt} width={60} height={60}/> 
+export function CardIcon({ src, alt }: Props) {
+  return <Image src={src} alt={alt} width={60} height={60} />;
+}
 
-    )
-} 
-
-export function DashLogo(){
-    return (
-        <Image src={dashlogo} alt={'logo'} priority={true}/>  
-        
-    )
-} 
+export function DashLogo() {
+  return <Image src="dashlogo.png" alt="dash logo" priority={true} />;
+}
