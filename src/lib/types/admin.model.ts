8a2135985294b828c59bdd78ccdf9e5287c2f00d
Wangariiisharon <mwangiiisharon@gmail.com>
2024-06-07 16:50:03 +0300

    Fix notifications endless loop and update AuthProvider

---
+++ src/lib/types/admin.model.ts
@@ -0,0 +1,15 @@
+export interface AdminUser {
+  docId: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  initials?: string;
+  phonenumber: string;
+  organisationId: string;
+  fcmToken: string;
+  super_admin: string;
+  adminId: string;
+  userId: string;
+  department: string;
+  additionalPermissions: string[];
+}
