eeb9ff2b5db8bc15ec5cade28256953d96395449
Wangariiisharon <mwangiiisharon@gmail.com>
2024-06-07 10:43:50 +0300

    remove untracked cached files e.g. '.firebase'

--- .firebase/truck-it-bf0b2/hosting/_next/static/chunks/pages/Vehicles/pending-5356e62e871bae61.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9448],{9038:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Vehicles/pending",function(){return u(2680)}])}},function(n){n.O(0,[5675,9647,9193,4159,9124,1510,117,2680,2888,9774,179],function(){return n(n.s=9038)}),_N_E=n.O()}]);
