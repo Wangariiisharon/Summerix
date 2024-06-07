c3328767f61772b8ca4ff3306ac698bd81b8c1cf
Wangariiisharon <mwangiiisharon@gmail.com>
2024-06-07 11:09:43 +0300

    update SiteNav component

--- src/components/images/index.tsx
+++ src/components/images/index.tsx
@@ -9,6 +9,3 @@ export function CardIcon({ src, alt }: Props) {
   return <Image src={src} alt={alt} width={60} height={60} />;
 }
 
-export function DashLogo() {
-  return <Image src="dashlogo.png" alt="dash logo" priority={true} />;
-}
