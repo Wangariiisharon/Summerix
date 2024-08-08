b8e7e13557562de83efc28b6b43b9f97704d5995
Sharon <sharon@gessic.com>
2024-08-08 13:23:52 +0300

    New Link

--- src/pages/Administration/Admins/manage_admins/Admins.tsx
+++ src/pages/Administration/Admins/manage_admins/Admins.tsx
@@ -425,8 +425,7 @@ export default function Admins() {
 
       // Send sign-in link to email
       const actionCodeSettings = {
-        url: `https://truckit-git-develop-gorafi.vercel.app/signin`,
-
+        url: `https://truckit-git-develop-gorafi.vercel.app/signin?_vercel_share=Sm5ZkWuZDso8NDfYmxUtegLqpGi09bPD`,
         handleCodeInApp: true,
       };
       await sendSignInLinkToEmail(auth, values.email, actionCodeSettings);
