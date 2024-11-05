b7b786994d35fac5fcfef28a653ca0f635c79916
Julius Githaiga <julius@gessic.com>
2024-11-05 12:44:06 +0300

    update AuthProvider component

--- src/components/Authentication/AuthProvider.tsx
+++ src/components/Authentication/AuthProvider.tsx
@@ -1,123 +1,3 @@
-// }
-// interface ParsedToken {
-//   role?: string;
-//   super_admin?: boolean;
-//   [key: string]: any;
-// }
-
-// const defaultContextValue: AuthContextType = {
-//   isAuthenticated: false,
-//   isSuperAdmin: false,
-//   currentAdmin: null,
-//   currentUser: null,
-//   userId: null,
-//   organisationId: null,
-//   userData: null,
-//   userClaims: null, // Add this line
-//   hasPermission: () => false,
-// };
-
-// export const AuthContext = createContext<AuthContextType>(defaultContextValue);
-
-// export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
-//   children,
-// }) => {
-//   const [currentAdmin, setCurrentAdmin] = useState<AdminUser | null>(null);
-//   const [currentUser, setCurrentUser] = useState<User | null>(null);
-//   const [userData, setUserData] = useState<AdminData | null>(null);
-//   const [userClaims, setUserClaims] = useState<ParsedToken | null>(null);
-
-//   const router = useRouter();
-
-//   const hasPermission = (permissionKey: string) =>
-//     userData?.departments.includes(permissionKey) ?? false;
-
-//   const isSuperAdmin = useMemo(() => {
-//     return userData?.super_admin ?? false;
-//   }, [userData]);
-
-//   useEffect(() => {
-//     const auth = getAuth(firebaseApp);
-//     const unsubscribe = onAuthStateChanged(auth, async (user) => {
-//       if (user) {
-//         setCurrentUser(user);
-
-//         try {
-//           // Force refresh the token to get updated claims
-//           await user.getIdToken(true);
-//           const token = await user.getIdToken();
-//           const uid = user.uid;
-
-//           console.log("User ID Token:", token);
-
-//           const res = await fetch(`/api/user?uid=${uid}`, {
-//             headers: {
-//               Authorization: `Bearer ${token}`,
-//             },
-//           });
-
-//           console.log("API Response:", res);
-
-//           if (!res.ok) {
-//             throw new Error("Failed to fetch user data");
-//           }
-
-//           const data = await res.json();
-//           setUserData(data);
-
-//           // Assuming custom claims are part of the user data fetched from the API
-//           const userClaims = data.customClaims || {};
-//           setUserClaims(userClaims);
-
-//           // Derive initials from firstname and lastname
-//           const adminUser = data as AdminUser;
-//           adminUser.initials =
-//             data.firstname?.charAt(0)?.toUpperCase() +
-//             data.lastname?.charAt(0)?.toUpperCase();
-//           setCurrentAdmin(adminUser);
-
-//           console.log("UserData from API:", data);
-//           console.log("userClaims Auth context:", userClaims);
-//         } catch (error) {
-//           console.error(error);
-//           setCurrentUser(null);
-//           setUserData(null);
-//           setUserClaims(null); // Reset userClaims
-//         }
-//       } else {
-//         console.log("redirect to signin page");
-//         setCurrentUser(null);
-//         setUserData(null);
-//         setUserClaims(null); // Reset userClaims
-//       }
-//     });
-
-//     return () => unsubscribe();
-//   }, [router]);
-
-//   return (
-//     <AuthContext.Provider
-//       value={{
-//         currentAdmin,
-//         currentUser,
-//         isAuthenticated: !!currentUser,
-//         userId: currentUser?.uid || null,
-//         organisationId: userData?.organisationId || null,
-//         userData,
-//         userClaims, // Add this line
-//         hasPermission,
-//         isSuperAdmin,
-//       }}
-//     >
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export function useAuthContext() {
-//   return useContext(AuthContext);
-// }
-
 import React, {
   createContext,
   useContext,
@@ -125,9 +5,16 @@ import React, {
   useEffect,
   useMemo,
 } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-import firebaseApp from "@/firebase/configs";
+import { onAuthStateChanged } from "firebase/auth";
+import {
+  doc,
+  onSnapshot,
+  collection,
+  query,
+  where,
+  limit,
+} from "firebase/firestore";
+import { fbAuth, fbDb } from "@/firebase/configs";
 import { AdminUser } from "@/lib/types/admin.model";
 import { useRouter } from "next/navigation";
 
@@ -153,7 +40,6 @@ interface AuthContextType {
   currentUser: User | null;
   userId: string | null;
   organisationId: string | null;
-  userData: AdminData | null;
   userClaims: ParsedToken | null;
   departmentData: any | null;
   hasPermission: (permissionKey: string) => boolean;
@@ -173,7 +59,6 @@ const defaultContextValue: AuthContextType = {
   currentUser: null,
   userId: null,
   organisationId: null,
-  userData: null,
   userClaims: null,
   departmentData: null,
   hasPermission: () => false,
@@ -186,158 +71,80 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   const [currentAdmin, setCurrentAdmin] = useState<AdminUser | null>(null);
   const [currentUser, setCurrentUser] = useState<User | null>(null);
-  const [userData, setUserData] = useState<AdminData | null>(null);
   const [userClaims, setUserClaims] = useState<ParsedToken | null>(null);
   const [departmentData, setDepartmentData] = useState<any | null>(null);
 
   const router = useRouter();
 
-  const hasPermission = (permissionKey: string) =>
-    departmentData?.permissions.includes(permissionKey) ?? false;
-
-  const isSuperAdmin = useMemo(() => {
-    return userData?.super_admin ?? false;
-  }, [userData]);
   useEffect(() => {
-    const auth = getAuth(firebaseApp);
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        setCurrentUser(user);
+    setCurrentUser(null);
+    setUserClaims(null);
 
-        try {
-          // Force refresh token to get updated custom claims
-          await user.getIdToken(true);
-          const token = await user.getIdToken();
-          const uid = user.uid;
+    const unsubscribe = onAuthStateChanged(fbAuth, async (fbUser) => {
+      console.debug("onAuthStateChanged > fbUser:", { uid: fbUser?.uid });
 
-          const res = await fetch(`/api/user?uid=${uid}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
+      if (fbUser) {
+        setCurrentUser(fbUser);
 
-          if (!res.ok) {
-            throw new Error("Failed to fetch user data");
-          }
-
-          const data = await res.json();
-          setUserData(data);
-
-          const userClaims = data.customClaims || {};
-          setUserClaims(userClaims);
-          console.log("userClaims AuthProvid:", userClaims);
-
-          // Fetch department data if departmentId exists
-          if (userClaims.departmentId) {
-            const firestore = getFirestore(firebaseApp);
-            const departmentDocRef = doc(
-              firestore,
-              "departments",
-              userClaims.departmentId
-            );
-            const departmentDoc = await getDoc(departmentDocRef);
-
-            if (departmentDoc.exists()) {
-              setDepartmentData(departmentDoc.data());
-            } else {
-              setDepartmentData(null);
-            }
-          } else {
-            setDepartmentData(null);
-          }
-
-          const adminUser = data as AdminUser;
-          adminUser.initials =
-            data.firstname?.charAt(0)?.toUpperCase() +
-            data.lastname?.charAt(0)?.toUpperCase();
-          setCurrentAdmin(adminUser);
-        } catch (error) {
-          console.error(error);
-          setCurrentUser(null);
-          setUserData(null);
-          setUserClaims(null);
-          setDepartmentData(null);
-          console.log("Error fetching user data Custom Claims:", error);
-        }
+        const tokenResult = await fbUser.getIdTokenResult();
+        console.debug("AuthProvider > tokenResult:", {
+          claims: tokenResult.claims,
+        });
+        setUserClaims(tokenResult.claims || {});
       } else {
-        setCurrentUser(null);
-        setUserData(null);
-        setUserClaims(null);
-        setDepartmentData(null);
-        console.log("Error fetching user data Custom Claims: Null");
+        console.debug("AuthProvider > redirect to signin");
+        router.push("/signin");
       }
     });
 
     return () => unsubscribe();
   }, [router]);
 
-  // useEffect(() => {
-  //   const auth = getAuth(firebaseApp);
-  //   const unsubscribe = onAuthStateChanged(auth, async (user) => {
-  //     if (user) {
-  //       setCurrentUser(user);
+  useEffect(() => {
+    setCurrentAdmin(null);
 
-  //       try {
-  //         await user.getIdToken(true);
-  //         const token = await user.getIdToken();
-  //         const uid = user.uid;
+    // Fetch department data if departmentId exists
+    if (currentUser && currentUser.uid) {
+      const colRef = collection(fbDb, "admins");
+      const unsubscribe = onSnapshot(
+        query(colRef, where("userId", "==", currentUser.uid), limit(1)),
+        async (snapshot) => {
+          const data = snapshot.docs[0].data() as AdminUser;
+          data.docId = snapshot.docs[0].id;
 
-  //         const res = await fetch(`/api/user?uid=${uid}`, {
-  //           headers: {
-  //             Authorization: `Bearer ${token}`,
-  //           },
-  //         });
+          data.initials =
+            data.firstname?.charAt(0)?.toUpperCase() +
+            data.lastname?.charAt(0)?.toUpperCase();
+          console.debug("AuthProvider > adminUser:", data);
 
-  //         if (!res.ok) {
-  //           throw new Error("Failed to fetch user data");
-  //         }
+          setCurrentAdmin(data);
+        }
+      );
+      return () => unsubscribe();
+    }
+  }, [currentUser]);
 
-  //         const data = await res.json();
-  //         setUserData(data);
+  useEffect(() => {
+    setDepartmentData(null);
 
-  //         const userClaims = data.customClaims || {};
-  //         setUserClaims(userClaims);
+    // Fetch department data if departmentId exists
+    if (userClaims && userClaims.departmentId) {
+      const docRef = doc(fbDb, "departments", userClaims.departmentId);
+      const unsubscribe = onSnapshot(docRef, async (snapshot) => {
+        if (snapshot.exists()) {
+          setDepartmentData(snapshot.data());
+        }
+      });
+      return () => unsubscribe();
+    }
+  }, [userClaims]);
 
-  //         if (userClaims.departmentId) {
-  //           const firestore = getFirestore(firebaseApp);
-  //           const departmentDocRef = doc(
-  //             firestore,
-  //             "departments",
-  //             userClaims.departmentId
-  //           );
-  //           const departmentDoc = await getDoc(departmentDocRef);
+  const hasPermission = (permissionKey: string) =>
+    departmentData?.permissions.includes(permissionKey) ?? false;
 
-  //           if (departmentDoc.exists()) {
-  //             setDepartmentData(departmentDoc.data());
-  //           } else {
-  //             setDepartmentData(null);
-  //           }
-  //         } else {
-  //           setDepartmentData(null);
-  //         }
-
-  //         const adminUser = data as AdminUser;
-  //         adminUser.initials =
-  //           data.firstname?.charAt(0)?.toUpperCase() +
-  //           data.lastname?.charAt(0)?.toUpperCase();
-  //         setCurrentAdmin(adminUser);
-  //       } catch (error) {
-  //         console.error(error);
-  //         setCurrentUser(null);
-  //         setUserData(null);
-  //         setUserClaims(null);
-  //         setDepartmentData(null);
-  //       }
-  //     } else {
-  //       setCurrentUser(null);
-  //       setUserData(null);
-  //       setUserClaims(null);
-  //       setDepartmentData(null);
-  //     }
-  //   });
-
-  //   return () => unsubscribe();
-  // }, [router]);
+  const isSuperAdmin = useMemo(() => {
+    return currentAdmin?.super_admin ?? false;
+  }, [currentAdmin]);
 
   return (
     <AuthContext.Provider
@@ -346,8 +153,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
         currentUser,
         isAuthenticated: !!currentUser,
         userId: currentUser?.uid || null,
-        organisationId: userData?.organisationId || null,
-        userData,
+        organisationId: currentAdmin?.organisationId || null,
         userClaims,
         departmentData,
         hasPermission,
