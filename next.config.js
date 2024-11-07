775681a7a734bd939ac9220978e5230d6049316c
Wangariiisharon <mwangiiisharon@gmail.com>
2024-11-07 14:43:07 +0300

    update @sidebar component

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/signin/",
+        destination: "/home",
         permanent: true,
       },
     ];
---
+++ src/app/(primary)/@sidebar/default.tsx
@@ -0,0 +1,9 @@
+import Sidebar from './page';
+
+export default function Default() {
+  return (
+    <main className="">
+      <Sidebar />
+    </main>
+  );
+}
