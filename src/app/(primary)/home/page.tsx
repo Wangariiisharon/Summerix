62f6129561e32951af644819e602e4bae9fa9227
Julius Githaiga <julius@gessic.com>
2024-11-07 22:21:58 +0300

    Added dashboard tripss overview

--- src/app/(primary)/home/page.tsx
+++ src/app/(primary)/home/page.tsx
@@ -21,6 +21,12 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Home() {
   const { organisationId } = useAuthContext();
+  const [tripStats, setTripStats] = useState({
+    total: 0,
+    booked: 0,
+    completed: 0,
+    cancelled: 0,
+  });
   const [vehicleStats, setVehicleStats] = useState({
     total: 0,
     available: 0,
@@ -44,31 +50,31 @@ export default function Home() {
     });
 
     const snapshot1 = await getAggregateFromServer(
-      query(q, where('availability_status', '==', 'Available')),
+      query(q, where("availability_status", "==", "Available")),
       {
         docsCount: count(),
-      },
+      }
     );
 
     const snapshot2 = await getAggregateFromServer(
-      query(q, where('availability_status', '==', 'On Route')),
+      query(q, where("availability_status", "==", "On Route")),
       {
         docsCount: count(),
-      },
+      }
     );
 
     const snapshot3 = await getAggregateFromServer(
-      query(q, where('availability_status', '==', 'Out Of Service')),
+      query(q, where("availability_status", "==", "Out Of Service")),
       {
         docsCount: count(),
-      },
+      }
     );
 
     const snapshot4 = await getAggregateFromServer(
-      query(q, where('availability_status', '==', 'Under Maintenance')),
+      query(q, where("availability_status", "==", "Under Maintenance")),
       {
         docsCount: count(),
-      },
+      }
     );
 
     setVehicleStats({
@@ -80,9 +86,53 @@ export default function Home() {
     });
   }, [organisationId]);
 
+  const doFilterTrips = useCallback(async () => {
+    if (!organisationId) return;
+
+    let q = query(
+      collection(fbDb, Constants.fbTrips),
+      where("organisationId", "==", organisationId)
+      // where("timestamp", ">=", Timestamp.fromDate(startDate)),
+      // where("timestamp", "<=", Timestamp.fromDate(endDate))
+    );
+
+    const snapshot = await getAggregateFromServer(q, {
+      docsCount: count(),
+    });
+
+    const snapshot1 = await getAggregateFromServer(
+      query(q, where("trip_status", "==", "Booked")),
+      {
+        docsCount: count(),
+      }
+    );
+
+    const snapshot2 = await getAggregateFromServer(
+      query(q, where("trip_status", "==", "Done")),
+      {
+        docsCount: count(),
+      }
+    );
+
+    const snapshot3 = await getAggregateFromServer(
+      query(q, where("trip_status", "==", "Cancelled")),
+      {
+        docsCount: count(),
+      }
+    );
+
+    setTripStats({
+      total: snapshot.data().docsCount,
+      booked: snapshot1.data().docsCount,
+      completed: snapshot2.data().docsCount,
+      cancelled: snapshot3.data().docsCount,
+    });
+  }, [organisationId]);
+
   useEffect(() => {
     doFilterVehicles();
-  }, [doFilterVehicles]);
+    doFilterTrips();
+  }, [doFilterTrips, doFilterVehicles]);
 
   return (
     <main className="">
@@ -123,9 +173,17 @@ export default function Home() {
 
       <section className="mt-5 grid-1-2 gap-5">
         <div className="p-4 bg-white rounded">
-          <h3 className="text-lg">Vehicles Overview</h3>
+          <div className="flex items-center justify-between gap-5">
+            <h3 className="text-lg">Vehicles Overview</h3>
+            <Link
+              href="/vehicles/"
+              className="btn btn-outline border-[#C0D7FA] font-light"
+            >
+              View All
+            </Link>
+          </div>
           <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 items-center gap-5">
-            <div className="col-span-1">
+            <div className="">
               <Doughnut
                 data={{
                   datasets: [
@@ -181,64 +239,96 @@ export default function Home() {
             <div className="">
               <p className="text-gray-500">Total</p>
               <p className="text-4xl">{vehicleStats.total}</p>
-              <div className="mt-5 grid gap-3">
-                <div className="flex items-center gap-5">
-                  <div className="p-2 bg-[#4FD1C5]/20 text-[#4FD1C5] rounded">
-                    <TruckIcon className="h-5 w-5" />
-                  </div>
-                  <p className="text-gray-500">Available</p>
-                </div>
-                <div className="flex items-center gap-5">
-                  <div className="p-2 bg-[#065AD8]/20 text-[#065AD8] rounded">
-                    <TruckIcon className="h-5 w-5" />
-                  </div>
-                  <p className="text-gray-500">On Route</p>
-                </div>
-                <div className="flex items-center gap-5">
-                  <div className="p-2 bg-[#FFC107]/20 text-[#FFC107] rounded">
-                    <TruckIcon className="h-5 w-5" />
-                  </div>
-                  <p className="text-gray-500">Under Maintenance</p>
-                </div>
-                <div className="flex items-center gap-5">
-                  <div className="p-2 bg-[#C80815]/20 text-[#C80815] rounded">
-                    <TruckIcon className="h-5 w-5" />
-                  </div>
-                  <p className="text-gray-500">Out of service</p>
-                </div>
+              <div className="mt-5 grid gap-1">
+                {[
+                  {
+                    name: "Available",
+                    value: vehicleStats.available,
+                    classNames: "bg-[#4FD1C5]/20 text-[#4FD1C5]",
+                  },
+                  {
+                    name: "On Route",
+                    value: vehicleStats.onRoute,
+                    classNames: "bg-[#065AD8]/20 text-[#065AD8]",
+                  },
+                  {
+                    name: "Under Maintenance",
+                    value: vehicleStats.underMaintenance,
+                    classNames: "bg-[#FFC107]/20 text-[#FFC107]",
+                  },
+                  {
+                    name: "Out Of Service",
+                    value: vehicleStats.outOfService,
+                    classNames: "bg-[#C80815]/20 text-[#C80815]",
+                  },
+                ].map(({ name, value, classNames }) => {
+                  return (
+                    <div
+                      key={name}
+                      className="flex items-center justify-between gap-5"
+                    >
+                      <div className="flex items-center gap-5">
+                        <div className={`p-2 rounded ${classNames}`}>
+                          <TruckIcon className="h-5 w-5" />
+                        </div>
+                        <p className="text-gray-500">{name}</p>
+                      </div>
+                      <p>{value}</p>
+                    </div>
+                  );
+                })}
+                <div className="hidden bg-[#FFC107]"></div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="p-4 bg-white rounded">
+        <div className="p-4 bg-white rounded">
           <div className="flex items-center justify-between gap-5">
-            <h3 className="text-lg">Out of service vehicles</h3>
+            <h3 className="text-lg">Trips Overview</h3>
             <Link
-              href="/vehicles/out-of-service"
+              href="/trips"
               className="btn btn-outline border-[#C0D7FA] font-light"
             >
               View All
             </Link>
           </div>
-          <div className="mt-5">
-            <div className="relative max-w-xs mx-auto w-full h-full">
+          <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 items-center gap-5">
+            <div className="">
               <Doughnut
                 data={{
                   datasets: [
                     {
-                      label: "Vehicles",
+                      label: "Booked",
                       data: [
-                        vehicleStats.outOfService,
-                        vehicleStats.total - vehicleStats.outOfService,
+                        tripStats.booked,
+                        tripStats.total - tripStats.booked,
+                      ],
+                      backgroundColor: ["#065AD8", "#E9ECEF"],
+                      borderWidth: 2,
+                    },
+                    {
+                      label: "Completed",
+                      data: [
+                        tripStats.completed,
+                        tripStats.total - tripStats.completed,
+                      ],
+                      backgroundColor: ["#4FD1C5", "#E9ECEF"],
+                      borderWidth: 2,
+                    },
+                    {
+                      label: "Cancelled",
+                      data: [
+                        tripStats.cancelled,
+                        tripStats.total - tripStats.cancelled,
                       ],
                       backgroundColor: ["#C80815", "#E9ECEF"],
-                      borderWidth: 1,
+                      borderWidth: 2,
                     },
                   ],
                 }}
                 options={{
                   responsive: true,
-                  cutout: "85%",
+                  cutout: "60%",
                   animation: {
                     animateScale: true,
                     easing: "easeInBack",
@@ -246,26 +336,46 @@ export default function Home() {
                   },
                 }}
               />
-              <div className="absolute top-32 left-32 z-20 text-center">
-                <p className="text-gray-500">Total Vehicles</p>
-                <p className="text-4xl font-bold">
-                  {vehicleStats.outOfService}
-                </p>
+            </div>
+            <div className="">
+              <p className="text-gray-500">Total</p>
+              <p className="text-4xl">{tripStats.total}</p>
+              <div className="mt-5 grid gap-1">
+                {[
+                  {
+                    name: "Booked",
+                    value: tripStats.booked,
+                    classNames: "bg-[#065AD8]/20 text-[#065AD8]",
+                  },
+                  {
+                    name: "Completed",
+                    value: tripStats.completed,
+                    classNames: "bg-[#4FD1C5]/20 text-[#4FD1C5]",
+                  },
+                  {
+                    name: "Cancelled",
+                    value: tripStats.cancelled,
+                    classNames: "bg-[#C80815]/20 text-[#C80815]",
+                  },
+                ].map(({ name, value, classNames }) => {
+                  return (
+                    <div
+                      key={name}
+                      className="flex items-center justify-between gap-5"
+                    >
+                      <div className="flex items-center gap-5">
+                        <div className={`p-2 rounded ${classNames}`}>
+                          <TruckIcon className="h-5 w-5" />
+                        </div>
+                        <p className="text-gray-500">{name}</p>
+                      </div>
+                      <p>{value}</p>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
-        </div> */}
-        <div className="p-4 bg-white rounded">
-          <div className="flex items-center justify-between gap-5">
-            <h3 className="text-lg">Trips Completed</h3>
-            <Link
-              href="/trips/completed"
-              className="btn btn-outline border-[#C0D7FA] font-light"
-            >
-              View All
-            </Link>
-          </div>
-          <div className=""></div>
         </div>
       </section>
     </main>
