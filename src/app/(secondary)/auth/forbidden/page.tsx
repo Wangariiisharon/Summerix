cb717cad7b9fd9fea0835aec8ee06c3729e9bcb2
Wangariiisharon <mwangiiisharon@gmail.com>
2024-11-15 14:00:13 +0300

    Added forbidden auth page

---
+++ src/app/(secondary)/auth/forbidden/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Forbidden',
+};
+
+export default function ForbiddenPage() {
+  return (
+    <main className="mt-10 flex items-center justify-center">
+      <div className="p-4 font-medium text-gray-600">
+        <div className="text-center text-5xl font-extrabold">403</div>
+        <p className="mt-5 text-red-500">You are not authorised to view this page.</p>
+      </div>
+    </main>
+  );
+}
