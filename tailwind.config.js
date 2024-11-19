f07cfc021e53409a6bbfab438704492661edba77
Julius Githaiga <julius@gessic.com>
2024-11-19 18:22:43 +0300

    updated eslint configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        primary: "#065AD8",
-        secondary: "#4FD1C5",
-        accent: "#030229",
-        danger: "#C80815",
+        primary: '#065AD8',
+        secondary: '#4FD1C5',
+        accent: '#030229',
+        danger: '#C80815',
       },
     },
   },
-  plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
+  plugins: [require('@tailwindcss/forms'), require('@headlessui/tailwindcss')],
 };
