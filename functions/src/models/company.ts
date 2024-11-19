236aaba95fc33b682e3c8f63d3cd25bd8c0985a9
Julius Githaiga <julius@gessic.com>
2024-11-19 17:19:54 +0300

    Added DRIVER model

--- functions/src/models/company.ts
+++ functions/src/models/company.ts
@@ -8,8 +8,10 @@ export interface COMPANY {
   docId: string;
 
   name: string;
-  description: string;
+  email: string;
   phoneNumber: string;
+  regNumber: string;
+  description: string;
   country: string;
   timezone: string;
   currency: string;
@@ -23,4 +25,12 @@ export interface COMPANY {
   lastUpdated: any;
   createdBy: { email: string; authId: string };
   updatedBy: { email: string; authId: string };
+}
+
+export interface COMPANY_DETAILS {
+  docId: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  regNumber: string;
 }
