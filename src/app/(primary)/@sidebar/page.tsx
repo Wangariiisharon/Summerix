f5b2204509bec6d155f008e2b9252109ce4dc43f
Julius Githaiga <julius@gessic.com>
2024-11-19 17:39:17 +0300

    Fix linting errors

--- src/app/(primary)/@sidebar/page.tsx
+++ src/app/(primary)/@sidebar/page.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client';
 
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 import {
   ChartBarIcon,
   DocumentChartBarIcon,
   HomeIcon,
   WrenchScrewdriverIcon,
-} from "@heroicons/react/24/outline";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import { usePathname } from "next/navigation";
-import { useMemo } from "react";
-import Link from "next/link";
+} from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+import Link from 'next/link';
 
 export default function Sidebar() {
   // const { authUser } = useAuthContext();
@@ -18,22 +18,22 @@ export default function Sidebar() {
 
   const navigation = useMemo(
     () => [
-      { name: "Dashboard", href: "/dashboard", icon: HomeIcon, visible: true },
+      { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, visible: true },
       {
-        name: "Administration",
-        href: "/administration",
+        name: 'Administration',
+        href: '/administration',
         icon: WrenchScrewdriverIcon,
         visible: true, // authUser?.isOwner || authUser?.isAdmin || false,
       },
       {
-        name: "Operations",
-        href: "/operations",
+        name: 'Operations',
+        href: '/operations',
         icon: ChartBarIcon,
         visible: true,
       },
       {
-        name: "Reports",
-        href: "/reports",
+        name: 'Reports',
+        href: '/reports',
         icon: DocumentChartBarIcon,
         visible: true,
       },
@@ -46,8 +46,8 @@ export default function Sidebar() {
       <Popover as="header" className="w-full" aria-label="Header">
         {({ open }) => (
           <>
-            <div className="-mt-7 p-4 w-auto flex lg:hidden z-20 bg-primary">
-              <div className="w-full flex justify-end">
+            <div className="z-20 -mt-7 flex w-auto bg-primary p-4 lg:hidden">
+              <div className="flex w-full justify-end">
                 <PopoverButton aria-label="Open menu" className="btn">
                   <span className="sr-only">Open menu</span>
                   {open ? (
@@ -61,11 +61,11 @@ export default function Sidebar() {
 
             <PopoverPanel
               as="nav"
-              className="block lg:hidden absolute top-10 z-20"
+              className="absolute top-10 z-20 block lg:hidden"
               aria-label="Site mobile navigation"
               anchor="bottom"
             >
-              <div className="w-full p-4 block gap-5 bg-gray-100">
+              <div className="block w-full gap-5 bg-gray-100 p-4">
                 {navigation
                   .filter((i) => i.visible)
                   .map(({ name, href, icon: ItemIcon }, index) => {
@@ -74,22 +74,16 @@ export default function Sidebar() {
                     return (
                       <Link key={index} href={href}>
                         <li
-                          className={`w-full flex border-r-4 hover:bg-blue-100 cursor-pointer ${
-                            isActive
-                              ? "border-primary bg-blue-100"
-                              : "border-transparent"
+                          className={`flex w-full cursor-pointer border-r-4 hover:bg-blue-100 ${
+                            isActive ? 'border-primary bg-blue-100' : 'border-transparent'
                           }`}
                         >
-                          <div className="w-full px-4 flex items-center justify-start">
+                          <div className="flex w-full items-center justify-start px-4">
                             <ItemIcon
-                              className={`h-7 w-7 ${
-                                isActive ? "text-primary" : "text-gray-500"
-                              }`}
+                              className={`h-7 w-7 ${isActive ? 'text-primary' : 'text-gray-500'}`}
                             />
                             <span
-                              className={`w-52 p-4 ${
-                                isActive ? "text-primary" : "text-gray-700"
-                              }`}
+                              className={`w-52 p-4 ${isActive ? 'text-primary' : 'text-gray-700'}`}
                             >
                               {name}
                             </span>
@@ -104,7 +98,7 @@ export default function Sidebar() {
         )}
       </Popover>
 
-      <ul className="hidden lg:flex flex-col">
+      <ul className="hidden flex-col lg:flex">
         {navigation
           .filter((i) => i.visible)
           .map(({ name, href, icon: ItemIcon }, index) => {
@@ -113,22 +107,16 @@ export default function Sidebar() {
             return (
               <Link key={index} href={href}>
                 <li
-                  className={`flex justify-center border-r-4 hover:bg-blue-100 cursor-pointer ${
-                    isActive
-                      ? "border-blue-500 bg-blue-100"
-                      : "border-transparent"
+                  className={`flex cursor-pointer justify-center border-r-4 hover:bg-blue-100 ${
+                    isActive ? 'border-blue-500 bg-blue-100' : 'border-transparent'
                   }`}
                 >
-                  <div className="w-full px-4 flex items-center justify-start">
+                  <div className="flex w-full items-center justify-start px-4">
                     <ItemIcon
-                      className={`h-7 w-7 ${
-                        isActive ? "text-blue-500" : "text-gray-500"
-                      }`}
+                      className={`h-7 w-7 ${isActive ? 'text-blue-500' : 'text-gray-500'}`}
                     />
                     <span
-                      className={`p-4 font-medium ${
-                        isActive ? "text-primary" : "text-gray-700"
-                      }`}
+                      className={`p-4 font-medium ${isActive ? 'text-primary' : 'text-gray-700'}`}
                     >
                       {name}
                     </span>
