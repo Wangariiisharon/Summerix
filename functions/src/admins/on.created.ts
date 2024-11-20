7f619805210e51b65d8bb780b28f8580b7602acc
Wangariiisharon <mwangiiisharon@gmail.com>
2024-11-21 02:07:12 +0300

    find corresponding client profile when admin is created

--- functions/src/admins/on.created.ts
+++ functions/src/admins/on.created.ts
@@ -4,6 +4,7 @@ import { doUpdateAuthClaims } from '../services/admin.service';
 import { getFirebaseUser } from '../services/auth.service';
 import Constants from '../Constants';
 import { ADMIN } from '../models/admin';
+import { getClientByDocId } from '../services/client.service';
 
 export const OnAdminCreated = runWith({
   maxInstances: 10,
@@ -25,6 +26,38 @@ export const OnAdminCreated = runWith({
       }
 
       await doUpdateAuthClaims(authUser.uid, admin);
+
+      // find corresponding client profile
+      const clientSnaphot = await getClientByDocId(docId);
+      if (!clientSnaphot.exists) {
+        logger.debug('Create client profile...');
+
+        await db()
+          .collection(Constants.fbClients)
+          .doc(authUser.uid)
+          .set({
+            email: admin.email,
+            company: admin.company || {},
+            idNumber: admin.idNumber || '',
+            phoneNumber: admin.phoneNumber || '',
+            firstName: admin.firstName,
+            lastName: admin.lastName,
+            displayName: admin.displayName || '',
+            photoURL: admin.photoURL || '',
+
+            createdBy: {
+              authId: 'functions',
+              email: 'functions@truckmate.com',
+            },
+            updatedBy: {
+              authId: 'functions',
+              email: 'functions@truckmate.com',
+            },
+
+            dateCreated: db.FieldValue.serverTimestamp(),
+            lastUpdated: db.FieldValue.serverTimestamp(),
+          });
+      }
     } catch (error) {
       logger.error('OnAdminCreated error:::', error);
     }
