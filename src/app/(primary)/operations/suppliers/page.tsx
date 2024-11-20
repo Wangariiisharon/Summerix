39a266d5ea6901bf7f08807d0714e139cb979f4b
Wangariiisharon <mwangiiisharon@gmail.com>
2024-11-21 01:04:36 +0300

    Added /operations main layout and page routes

---
+++ src/app/(primary)/operations/suppliers/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function Suppliers() {
+  return (
+    <main className="">
+      <h2 className="font-bold">Suppliers</h2>
+      <p className="mt-5">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi eos neque laborum corporis
+        dolore molestias. Modi unde in magnam neque corporis nam, rerum, optio aspernatur ipsam
+        minima ad accusamus recusandae!
+      </p>
+    </main>
+  );
+}
