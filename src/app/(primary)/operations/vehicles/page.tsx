4aca56bd134432c6f15c233d42c6c5f5d57861a5
Wangariiisharon <mwangiiisharon@gmail.com>
2024-11-21 10:20:08 +0300

    Added vehicles list page layout

--- src/app/(primary)/operations/vehicles/page.tsx
+++ src/app/(primary)/operations/vehicles/page.tsx
@@ -1,14 +1,143 @@
 'use client';
 
+import { useAuthContext } from '@/app/auth-provider';
+import RemotePagination from '@/components/remote-pagination';
+import Constants from '@/Constants';
+import useVehicles from '@/hooks/useVehicles';
+import { PARAMS_MAP } from '@/models/params-map';
+import { VEHICLE } from '@/models/vehicle';
+import { PencilSquareIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { DocumentSnapshot } from 'firebase/firestore';
+import moment from 'moment-timezone';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
 export default function Vehicles() {
+  const { authUser } = useAuthContext();
+  const [params, setParams] = useState<PARAMS_MAP>({
+    max: Constants.defaultPageSize,
+    orderBy: 'lastUpdated',
+    direction: 'desc',
+  });
+  const { count, vehicles } = useVehicles({
+    companyId: authUser?.companyId || 'xyz',
+    docId: null,
+    params,
+  });
+  const cursors = useRef<Map<number, DocumentSnapshot>>(new Map());
+  const [max, setMax] = useState(Constants.defaultPageSize);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    setParams({
+      orderBy: 'lastUpdated',
+      direction: 'desc',
+      max: max,
+
+      cursor: cursors.current.get(currentPage),
+    });
+  }, [currentPage, max]);
+
+  const onPageChanged = useCallback(
+    (nextPage: number) => {
+      setCurrentPage((page) => {
+        // first, we save the last document as page's cursor
+        cursors.current.set(page + 1, vehicles[vehicles.length - 1]?.doc);
+
+        // then we update the state with the next page's number
+        return nextPage;
+      });
+    },
+    [vehicles],
+  );
+
   return (
     <main className="">
-      <h2 className="font-bold">Vehicles</h2>
-      <p className="mt-5">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi eos neque laborum corporis
-        dolore molestias. Modi unde in magnam neque corporis nam, rerum, optio aspernatur ipsam
-        minima ad accusamus recusandae!
-      </p>
+      <section className="flex flex-col justify-between gap-5 sm:flex-row">
+        <div className="">
+          <h2 className="font-bold">Vehicles</h2>
+          <p className="text-gray-500">Manage company vehicles.</p>
+        </div>
+        <Link href="/operations/vehicles/new">
+          <div className="btn btn-flex btn-secondary">
+            <PlusIcon className="h-5 w-5" />
+            <p>Add Vehicle</p>
+          </div>
+        </Link>
+      </section>
+
+      <hr className="my-5" />
+
+      <div className="table-wrapper">
+        <div className="table-scroll text-sm">
+          <table className="my-table">
+            <thead className="sticky top-0">
+              <tr className="tr-header">
+                <th className="th"></th>
+                <th className="th text-left">Name</th>
+                <th className="th table-cell-sm">Reg. Number</th>
+                <th className="th table-cell-xl">Status</th>
+                <th className="th table-cell-xl">Last Modified</th>
+                <th className="th">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {vehicles.map((vehicle: VEHICLE) => {
+                return (
+                  <tr key={vehicle.docId} className="tr-body">
+                    <td className="td text-center">
+                      <div className="h-auto w-auto overflow-hidden rounded-xl">
+                        <Image
+                          src={vehicle.photoURL}
+                          alt={`${vehicle.name} image`}
+                          className="h-16 w-16 rounded-xl object-cover"
+                          height={50}
+                          width={50}
+                          priority
+                        />
+                      </div>
+                    </td>
+                    <td className="td text-left">
+                      <p>{vehicle.name}</p>
+                      <div className="mt-1">
+                        <p className="block sm:hidden">{vehicle.make}</p>
+                        <p className="block lg:hidden">{vehicle.model}</p>
+                      </div>
+                    </td>
+                    <td className="td table-cell-sm">{vehicle.regNumber}</td>
+                    <td className="td table-cell-xl">
+                      <div className="flex justify-center">
+                        <p className="w-fit rounded-full bg-gray-300 px-4 py-2">{vehicle.status}</p>
+                      </div>
+                    </td>
+                    <td className="td table-cell-xl">
+                      {vehicle.lastUpdated &&
+                        moment(vehicle.lastUpdated.toDate()).format(Constants.dateTimeFormat)}
+                    </td>
+                    <td className="td">
+                      <div className="td-actions">
+                        <Link href={`/operations/vehicles/${vehicle.docId}`}>
+                          <PencilSquareIcon className="h-5 w-5 text-primary hover:text-secondary" />
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="mx-2 my-5 mb-36">
+          <RemotePagination
+            max={max}
+            setMax={setMax}
+            itemsCount={count}
+            currentPage={currentPage}
+            pageChanged={onPageChanged}
+          />
+        </div>
+      </div>
     </main>
   );
 }
