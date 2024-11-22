bd214ac7a71f37474f6a8e0f9854e4af9dcb6797
Julius Githaiga <julius@gessic.com>
2024-11-22 09:46:24 +0300

    Added drivers list page layout

--- src/app/(primary)/operations/drivers/page.tsx
+++ src/app/(primary)/operations/drivers/page.tsx
@@ -1,14 +1,139 @@
 'use client';
 
+import { useAuthContext } from '@/app/auth-provider';
+import RemotePagination from '@/components/remote-pagination';
+import Constants from '@/Constants';
+import useDrivers from '@/hooks/useDrivers';
+import { DRIVER } from '@/models/driver';
+import { PARAMS_MAP } from '@/models/params-map';
+import { PencilSquareIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { DocumentSnapshot } from 'firebase/firestore';
+import moment from 'moment-timezone';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
 export default function Drivers() {
+  const { authUser } = useAuthContext();
+  const [params, setParams] = useState<PARAMS_MAP>({
+    max: Constants.defaultPageSize,
+    orderBy: 'lastUpdated',
+    direction: 'desc',
+  });
+  const { count, drivers } = useDrivers({
+    companyId: authUser?.companyId || 'xyz',
+    docId: null,
+    params,
+  });
+  const cursors = useRef<Map<number, DocumentSnapshot>>(new Map());
+  const [max, setMax] = useState(Constants.defaultPageSize);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    setParams({
+      orderBy: 'lastUpdated',
+      direction: 'desc',
+      max: max,
+
+      cursor: cursors.current.get(currentPage),
+    });
+  }, [currentPage, max]);
+
+  const onPageChanged = useCallback(
+    (nextPage: number) => {
+      setCurrentPage((page) => {
+        // first, we save the last document as page's cursor
+        cursors.current.set(page + 1, drivers[drivers.length - 1]?.doc);
+
+        // then we update the state with the next page's number
+        return nextPage;
+      });
+    },
+    [drivers],
+  );
+
   return (
     <main className="">
-      <h2 className="font-bold">Drivers</h2>
-      <p className="mt-5">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi eos neque laborum corporis
-        dolore molestias. Modi unde in magnam neque corporis nam, rerum, optio aspernatur ipsam
-        minima ad accusamus recusandae!
-      </p>
+      <section className="flex flex-col justify-between gap-5 sm:flex-row">
+        <div className="">
+          <h2 className="font-bold">Drivers</h2>
+          <p className="text-gray-500">Manage company drivers.</p>
+        </div>
+        <Link href="/administration/drivers/new">
+          <div className="btn btn-flex btn-secondary">
+            <PlusIcon className="h-5 w-5" />
+            <p>Add Driver</p>
+          </div>
+        </Link>
+      </section>
+
+      <hr className="my-5" />
+
+      <div className="table-wrapper">
+        <div className="table-scroll text-sm">
+          <table className="my-table">
+            <thead className="sticky top-0">
+              <tr className="tr-header">
+                <th className="th"></th>
+                <th className="th text-left">Name</th>
+                <th className="th table-cell-sm">Email Address</th>
+                <th className="th table-cell-xl">Phone Number</th>
+                <th className="th table-cell-xl">Last Modified</th>
+                <th className="th">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {drivers.map((driver: DRIVER) => {
+                return (
+                  <tr key={driver.docId} className="tr-body">
+                    <td className="td text-center">
+                      <div className="h-auto w-auto overflow-hidden rounded-xl">
+                        <Image
+                          src={driver.photoURL}
+                          alt={`${driver.displayName} image`}
+                          className="h-16 w-16 rounded-xl object-cover"
+                          height={50}
+                          width={50}
+                          priority
+                        />
+                      </div>
+                    </td>
+                    <td className="td text-left">
+                      <p>{driver.displayName}</p>
+                      <div className="mt-1">
+                        <p className="block sm:hidden">{driver.email}</p>
+                        <p className="block lg:hidden">{driver.phoneNumber}</p>
+                      </div>
+                    </td>
+                    <td className="td table-cell-sm">{driver.email}</td>
+                    <td className="td table-cell-xl">{driver.phoneNumber}</td>
+                    <td className="td table-cell-xl">
+                      {driver.lastUpdated &&
+                        moment(driver.lastUpdated.toDate()).format(Constants.dateTimeFormat)}
+                    </td>
+                    <td className="td">
+                      <div className="td-actions">
+                        <Link href={`/administration/users/${driver.docId}`}>
+                          <PencilSquareIcon className="h-5 w-5 text-primary hover:text-secondary" />
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="mx-2 my-5 mb-36">
+          <RemotePagination
+            max={max}
+            setMax={setMax}
+            itemsCount={count}
+            currentPage={currentPage}
+            pageChanged={onPageChanged}
+          />
+        </div>
+      </div>
     </main>
   );
 }
