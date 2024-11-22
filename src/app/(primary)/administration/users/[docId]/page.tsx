3272fe7f007d96353b3f6408dd9c4db3d9c9e959
Wangariiisharon <mwangiiisharon@gmail.com>
2024-11-22 13:28:19 +0300

    Fix linting errors

--- src/app/(primary)/administration/users/[docId]/page.tsx
+++ src/app/(primary)/administration/users/[docId]/page.tsx
@@ -111,7 +111,7 @@ export default function User({ params }: Props) {
 
     try {
       formValues.displayName = `${formValues.firstName.trim()} ${formValues.lastName.trim()}`;
-      
+
       if (docId === 'new') {
         const colRef = collection(fbDb, Constants.fbAdmins);
         await addDoc(colRef, {
