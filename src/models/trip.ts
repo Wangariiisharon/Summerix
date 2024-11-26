c4355e8140279c3b3e07c7d6129141854971232f
Julius Githaiga <julius@gessic.com>
2024-11-26 16:56:03 +0300

    Added trips list layout and refs

--- src/models/trip.ts
+++ src/models/trip.ts
@@ -3,6 +3,8 @@ import { type DocumentData, DocumentReference, QueryDocumentSnapshot } from 'fir
 import { CLIENT_DETAILS } from './client';
 import { COMPANY_DETAILS } from './company';
 import { DRIVER_DETAILS } from './driver';
+import { VEHICLE_DETAILS } from './vehicle';
+import { ADDRESS } from './address';
 
 export interface TRIP {
   docId: string;
@@ -12,26 +14,9 @@ export interface TRIP {
   company: COMPANY_DETAILS;
   client: CLIENT_DETAILS; // requested by
   driver: DRIVER_DETAILS;
-  vehicle: string; // VEHICLE_DETAILS
-
-  addedBy: {
-    docId: string;
-    name: string;
-    email: string;
-    phonenumber: string;
-  };
-  requestedBy: {
-    docId: string;
-    name: string;
-    email: string;
-    phonenumber: string;
-  };
-
-  from: any; // pick-up location details
-  to: any; // drop-off location details
-
-  started: any; // start details e.g. time
-  ended: any; // end details e.g. time
+  vehicle: VEHICLE_DETAILS;
+  from: ADDRESS; // pick-up location details
+  to: ADDRESS; // drop-off location details
   cargo: any; // cargo details e.g. size, type, quantity
   documents: {
     interChange: string;
@@ -54,6 +39,19 @@ export interface TRIP {
   doc: QueryDocumentSnapshot<DocumentData, DocumentData>;
   docRef: DocumentReference<DocumentData, DocumentData>;
 
+  createdBy: {
+    authId: string;
+    email: string;
+    displayName: string;
+  };
+  updatedBy: {
+    authId: string;
+    email: string;
+    displayName: string;
+  };
+
+  startedAt: any; // start details e.g. time
+  endedAt: any; // end details e.g. time
   dateCreated: any;
   lastUpdated: any;
 }
