434aff9a87d7999a6b80e018aae760cab2835991
Julius Githaiga <julius@gessic.com>
2024-11-27 17:49:01 +0300

    Added tripsGroup trigger functions

---
+++ functions/src/trips/on.deleted.ts
@@ -0,0 +1,19 @@
+import { logger, runWith } from 'firebase-functions/v1';
+import Constants from '../Constants';
+import { TRIP } from '../models/trip';
+
+export const OnTripDeleted = runWith({
+  maxInstances: 10,
+})
+  .firestore.document(`/${Constants.fbTrips}/{docId}`)
+  .onDelete(async (snapshot, context) => {
+    const trip = snapshot.data() as TRIP;
+    trip.docId = context.params.docId;
+    logger.log('OnTripDeleted > trip:', trip);
+
+    try {
+      // TODO: implement this
+    } catch (error) {
+      logger.error('OnTripDeleted error:', error);
+    }
+  });
