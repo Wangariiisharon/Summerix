a78e3cbf9a3430df43b0a483ae3b54450da4c919
Wangariiisharon <mwangiiisharon@gmail.com>
2024-11-29 11:44:21 +0300

    Added MAINTENANCE model

---
+++ src/models/maintenance.ts
@@ -0,0 +1,35 @@
+import {
+  type DocumentData,
+  DocumentReference,
+  QueryDocumentSnapshot,
+  Timestamp,
+} from 'firebase/firestore';
+import { COMPANY_DETAILS } from './company';
+import { VEHICLE_DETAILS } from './vehicle';
+import { SUPPLIER_DETAILS } from './supplier';
+
+export interface MAINTENANCE {
+  docId: string;
+  status: string; // select options
+
+  company: COMPANY_DETAILS;
+  vehicle: VEHICLE_DETAILS;
+  supplier: SUPPLIER_DETAILS;
+  jobCards: {
+    name: string;
+    value: string;
+  }[];
+  schedule: {
+    startAt: Timestamp;
+    endAt: Timestamp;
+  };
+  notes: string;
+
+  docRef: DocumentReference<DocumentData, DocumentData>;
+  doc: QueryDocumentSnapshot<DocumentData, DocumentData>;
+
+  dateCreated: any;
+  lastUpdated: any;
+  createdBy: { email: string; authId: string };
+  updatedBy: { email: string; authId: string };
+}
