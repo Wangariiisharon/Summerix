43c3745a675e0032ae27a4ef63e25c7e216b1995
Wangariiisharon <mwangiiisharon@gmail.com>
2024-12-05 13:38:28 +0300

    Added PARAMS_FILTER interface

---
+++ src/models/params-filter.ts
@@ -0,0 +1,9 @@
+import { Timestamp } from 'firebase/firestore';
+
+export interface PARAMS_FILTER {
+  dateRange: string;
+  filterByDate: string;
+  startDate: Timestamp;
+  endDate: Timestamp;
+}
+
