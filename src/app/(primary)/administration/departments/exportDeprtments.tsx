b5137a311b780678824fff72eda2b147353376cb
Wangariiisharon <mwangiiisharon@gmail.com>
2024-12-16 11:04:28 +0300

    Export Departmens update

---
+++ src/app/(primary)/administration/departments/exportDeprtments.tsx
@@ -0,0 +1,53 @@
+import { fbDb } from '@/firebase/configs';
+import { getDocs, collection, DocumentData } from 'firebase/firestore';
+import Constants from '@/Constants';
+
+export async function exportDataToCSV(companyId: string, status?: string) {
+  const tripsCollection = collection(fbDb, Constants.fbDepartments);
+  const snapshot = await getDocs(tripsCollection);
+  let data: DocumentData[] = [];
+
+  snapshot.forEach((doc) => {
+    const docData = doc.data();
+    if (docData.company.docId === companyId) {
+      data.push({
+        ...docData,
+        docId: doc.id,
+      });
+    }
+  });
+
+  if (data.length === 0) {
+    return 'No data found in Firestore.';
+  }
+
+  if (status && status !== 'all') {
+    data = data.filter((department) => {
+      switch (status) {
+        case 'all':
+          return true;
+        case 'active':
+          return department.isActive === true;
+        case 'inactive':
+          return department.isActive === false;
+
+        default:
+          return false;
+      }
+    });
+
+    if (data.length === 0) {
+      return `No data found for the status: ${status}.`;
+    }
+  }
+
+  const csvData = data.map((department) => ({
+    Name: department.name,
+    Status: department.isActive ? 'Active' : 'Inactive',
+  }));
+
+  const header = 'Name,Status';
+  const csvString = [header, ...csvData.map((item) => Object.values(item).join(','))].join('\n');
+
+  return csvString;
+}
