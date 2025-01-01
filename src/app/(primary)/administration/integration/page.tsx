2e94d41a998fe9a10fd31499eb9a620687f74ff2
@gessic-apps <abdul.osman@gessic.com>
2025-01-01 21:41:43 +0200

    show quickbooks integration coming soon

--- src/app/(primary)/administration/integration/page.tsx
+++ src/app/(primary)/administration/integration/page.tsx
@@ -4,11 +4,18 @@ export default function Integration() {
   return (
     <main className="-mx-4 rounded bg-white p-4">
       <h2 className="font-bold">Integration</h2>
-      <p className="mt-5">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi eos neque laborum corporis
-        dolore molestias. Modi unde in magnam neque corporis nam, rerum, optio aspernatur ipsam
-        minima ad accusamus recusandae!
-      </p>
+      <div className="mt-8 flex">
+        <div className="rounded-lg border border-gray-200 p-6 shadow-sm transition-shadow hover:shadow-md">
+          <div className="flex flex-col items-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/7/79/Intuit_QuickBooks_logo.svg"
+              alt="QuickBooks"
+              className="mb-4 h-16 w-auto"
+            />
+            <span className="font-medium text-gray-500">Coming soon</span>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
