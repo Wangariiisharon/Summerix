43aafe16b8b4d592f4a1a4c87e6299b81dd0d92d
Wangariiisharon <mwangiiisharon@gmail.com>
2025-01-01 18:59:13 +0200

    fix bugs, improve styles, added types, refactor countryList functions

--- src/firebase/configs.ts
+++ src/firebase/configs.ts
@@ -5,17 +5,17 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const apps = getApps();
-
+console.debug('apps:', apps);
 if (!apps.length || apps.length === 0) {
   console.debug('do initialize firebase...');
 
   try {
     initializeApp(clientConfig);
   } catch (error) {
-    console.error('FIREBASE INIT ERROR:::', error);
+    console.error('FIREBASE INIT ERROR:::', error); 
   }
 }
-
+console.debug('apps:', getApps());
 const firebaseApp = getApps()[0];
 export const fbDb = getFirestore(firebaseApp);
 export const fbAuth = getAuth(firebaseApp);
