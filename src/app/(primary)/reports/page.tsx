62506f6e85b7b8768cb0ca66473a02d16903517f
@gessic-apps <abdul.osman@gessic.com>
2025-01-16 14:28:29 +0200

    implement dashboard stats

--- src/app/(primary)/reports/page.tsx
+++ src/app/(primary)/reports/page.tsx
@@ -3,11 +3,5 @@
 import React from 'react';
 
 export default function Reports() {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat.
-    </div>
-  );
+  return <div></div>;
 }
