350ec52e7fe42dbda4cbaaed3899468e8f2b1b0d
Wangariiisharon <mwangiiisharon@gmail.com>
2025-05-26 18:32:59 +0300

    Changes

--- src/app/(secondary)/auth/layout.tsx
+++ src/app/(secondary)/auth/layout.tsx
@@ -19,12 +19,12 @@ export default function Layout({ children }: Props) {
   return (
     <main className="flex h-full min-h-screen w-full flex-row">
       <div className="hidden w-3/5 lg:block">
-      <div className="flex h-screen w-full"
+        <div
+          className="flex h-screen w-full"
           style={{
             background: 'linear-gradient(to bottom, #1C1967, #B26026, #FFFFFF)',
           }}
-        > 
-        </div>
+        ></div>
       </div>
       <div className="m-auto grid w-full max-w-sm lg:w-2/5">
         <div className="mt-10 flex flex-col p-4">
